18bf01cc866e86520b39a2054d5ea542048a5323
clementRbl <clement.rbl@gmail.com>
2020-10-27 12:56:44 +0100

    add signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
@@ -13,4 +13,13 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
      return this.userRepository.signUp(authCredentialsDto);
   }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+    
+    if (!username) {
+      throw new UnauthorizedException('Identifiant ou mot de passe incorrect');
+    }
+  }
+
 }
